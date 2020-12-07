3a12ca4363d338e2fa8169db9e202f14b9475678
huydang2k <huydang2k@gmail.com>
2020-12-07 15:34:59 +0700

    finish model

--- PRJ_CNPM/src/app/model/ThanhVienCuaHo.java
+++ PRJ_CNPM/src/app/model/ThanhVienCuaHo.java
@@ -1,5 +1,40 @@
 package app.model;
 
 public class ThanhVienCuaHo {
-    
+    private Integer idNhanKhau;
+    private Integer idHoKhau;
+    private String quanHeVoiChuHo;
+
+    public Integer getIdNhanKhau() {
+        return idNhanKhau;
+    }
+
+    public void setIdNhanKhau(Integer idNhanKhau) {
+        this.idNhanKhau = idNhanKhau;
+    }
+
+    public Integer getIdHoKhau() {
+        return idHoKhau;
+    }
+
+    public void setIdHoKhau(Integer idHoKhau) {
+        this.idHoKhau = idHoKhau;
+    }
+
+    public String getQuanHeVoiChuHo() {
+        return quanHeVoiChuHo;
+    }
+
+    public void setQuanHeVoiChuHo(String quanHeVoiChuHo) {
+        this.quanHeVoiChuHo = quanHeVoiChuHo;
+    }
+
+    public ThanhVienCuaHo(Integer idNhanKhau, Integer idHoKhau, String quanHeVoiChuHo) {
+        this.idNhanKhau = idNhanKhau;
+        this.idHoKhau = idHoKhau;
+        this.quanHeVoiChuHo = quanHeVoiChuHo;
+    }
+
+    public ThanhVienCuaHo() {
+    }
 }
