94a3f8ec856e5b7430ca1b7203e430f05b776c1c
phamnam1805 <57164967+phamnam1805@users.noreply.github.com>
2020-12-10 20:40:02 +0700

    Update DatabaseConfig.java

--- PRJ_CNPM/src/app/common/DatabaseConfig.java
+++ PRJ_CNPM/src/app/common/DatabaseConfig.java
@@ -4,5 +4,5 @@ public interface DatabaseConfig {
      String DRIVER = "com.mysql.cj.jdbc.Driver"; // chứa đường dẫn đến thư viện jdbc
      String URL_DATABASE = "jdbc:mysql://localhost:3306/quan_ly_pt_pq"; // đường dẫn để kết nối đến schema
      String USERNAME = "root";
-     String PASSWORD = "Nam123456Nam"; // mật khẩu của mysql
+     String PASSWORD = "root"; // mật khẩu của mysql
 }
